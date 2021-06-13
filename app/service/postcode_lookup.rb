559562cceb4bcf6a8655628b4e7a448b5e1d08ae
Chris Chamberlain <christopher.chamberlain@simplybusiness.co.uk>
2021-06-13 07:31:12 +0100

    Adding an extact method

--- app/service/postcode_lookup.rb
+++ app/service/postcode_lookup.rb
@@ -6,4 +6,12 @@ class PostcodeLookup
   def clean_postcode(postcode)
     postcode.downcase.gsub(/\s+/, '')
   end
+
+  def extract_lsoa(res)
+    if res['status'] == 200
+      res['result']['lsoa']
+    elsif res['status'] == 404 && res['error'] == 'Postcode not found'
+      'Postcode not found'
+    end
+  end
 end
--- spec/service/postcode_lookup_spec.rb
+++ spec/service/postcode_lookup_spec.rb
@@ -5,9 +5,22 @@ require 'rails_helper'
 RSpec.describe PostcodeLookup do
   subject(:lookup) { described_class.new }
 
+  let(:unfound_result) { { 'status' => 404, 'error' => 'Postcode not found' } }
+  let(:found_result) { { 'status' => 200, 'result' => { 'postcode' => 'SE1 7QD', 'lsoa' => 'Southwark 034A' } } }
+
   context '#clean_postcode' do
     it 'removes spaces and downcases the postcode' do
       expect(lookup.clean_postcode('BN1 1AL')).to eq 'bn11al'
+    end
+  end
+
+  context '#extract_lsoa' do
+    it 'extracts the lsoa if found' do
+      expect(lookup.extract_lsoa(found_result)).to eq 'Southwark 034A'
+    end
+
+    it 'returns a not found message if not found' do
+      expect(lookup.extract_lsoa(unfound_result)).to eq 'Postcode not found'
     end
   end
 end
