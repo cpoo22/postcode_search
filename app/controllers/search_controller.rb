10cc1a9cb643f12d008c31a56c41123b436e764d
Chris Chamberlain <christopher.chamberlain@simplybusiness.co.uk>
2021-06-13 10:53:48 +0100

    Adding search and specs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,4 +3,25 @@
 class SearchController < ApplicationController
   # GET /search
   def new; end
+
+  # POST /search
+  def search
+    @postcode = search_params
+
+    @postcode_allowed = rules.allow?(@postcode)
+
+    respond_to do |format|
+      format.html { render :new }
+    end
+  end
+
+  private
+
+  def search_params
+    params.require(:postcode)
+  end
+
+  def rules
+    @rules ||= PostcodeRules.new
+  end
 end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -5,4 +5,32 @@ feature 'Search' do
     visit '/search'
     expect(page).to have_content 'Search'
   end
+
+  scenario 'Searching for a not allowed postcode' do
+    visit '/search'
+    fill_in 'postcode', with: 'BN1 1AL'
+    click_button 'Look up Postcode'
+    expect(page).to have_content 'Boo hiss boo'
+  end
+
+  scenario 'Searching for an allowed Sothwark postcode' do
+    visit '/search'
+    fill_in 'postcode', with: 'SE1 7QD'
+    click_button 'Look up Postcode'
+    expect(page).to have_content 'Hurrah'
+  end
+
+  scenario 'Searching for an allowed Lambeth postcode' do
+    visit '/search'
+    fill_in 'postcode', with: 'SW2 5JD'
+    click_button 'Look up Postcode'
+    expect(page).to have_content 'Hurrah'
+  end
+
+  scenario 'Searching for a not found postcode' do
+    visit '/search'
+    fill_in 'postcode', with: 'SH24 1AA'
+    click_button 'Look up Postcode'
+    expect(page).to have_content 'Hurrah'
+  end
 end
